d67afaccfb64396675caf4e07acfddf9eff68e14
Tarun kumar <115946244+krtarun7@users.noreply.github.com>
2026-08-07 12:28:02 +0530

    Update database.py

--- Backend/app/database/database.py
+++ Backend/app/database/database.py
@@ -1,15 +1,24 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:root@localhost/resume_analyzer"
+# Load environment variables
+load_dotenv()
 
+# Read DATABASE_URL from .env
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create database engine
 engine = create_engine(DATABASE_URL)
 
+# Database session
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
 
+# Base model
 Base = declarative_base()
